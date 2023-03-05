d185a0ce6a3bffecb57168334c52db0444da0cf7
vitor <vitorsantosnavarro@gmail.com>
2023-03-05 20:23:07 -0300

    Inicio projeto do vini

---
+++ src/services/database.mjs
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore,collection, addDoc, getDocs } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDxoUVvZeks-qFpc0ilgjpDKUR60fu8FDg",
+  authDomain: "projeto-vini-974c4.firebaseapp.com",
+  databaseURL: "https://projeto-vini-974c4-default-rtdb.firebaseio.com",
+  projectId: "projeto-vini-974c4",
+  storageBucket: "projeto-vini-974c4.appspot.com",
+  messagingSenderId: "1003540098978",
+  appId: "1:1003540098978:web:b10dfd7adc49394b828cfb",
+  measurementId: "G-32L52F61MT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const querySnapshot = await getDocs(collection(db, "users"));
+querySnapshot.forEach((doc) => {
+  console.log(`${doc.id} => ${doc.data()}`);
+});
+
+
+
+
