fd7ad584ab14bda44bd23c9350dc188b32e071f8
vitor-navarro <vitorsantosnavarro@gmail.com>
2023-03-06 11:25:25 -0300

    atualização começo do projeto

---
+++ src/components/ProductForm/index.tsx
@@ -0,0 +1,26 @@
+export function CreateProduct() {
+    return(
+        <div>
+            <h1>Criar Produto</h1>
+
+            <div>
+                <form>
+                    <div>
+                        <label>Nome do produto</label>
+                        <input type="text" placeholder="Nome do produto" />
+                    </div>
+                    <div>
+                        <label>Valor Custo</label>
+                        <input type="text" placeholder="Valor custo" />
+                    </div>
+                    <div>
+                        <label>Descrição do produto</label>
+                        <input type="text" placeholder="Descrição do produto" />
+                    </div>
+                    <button type="submit">Criar produto</button>
+                </form>
+            </div>
+        </div>
+    );
+    
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
 
+const ProductForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  
+  & > div {
+    margin-bottom: 20px;
+  }
 
-export default function Home() {
+  & > div > label {
+    margin-bottom: 10px;
+    font-size: 16px;
+    font-weight: bold;
+  }
 
+  & > div > input {
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    font-size: 16px;
+    width: 100%;
+  }
+`;
+
+const ProductForm = () => {
   return (
-    <>
-      <h1>Header</h1>
-
+    <ProductForm>
       <div>
-      teste
+        <label>Nome do produto</label>
+        <input type="text" placeholder="Nome do produto" />
       </div>
-    </>
-  )
-}
+      <div>
+        <label>Valor Custo</label>
+        <input type="text" placeholder="Valor custo" />
+      </div>
+      <div>
+        <label>Descrição do produto</label>
+        <input type="text" placeholder="Descrição do produto" />
+      </div>
+    </ProductForm>
+  );
+};
+
+export default ProductForm;
