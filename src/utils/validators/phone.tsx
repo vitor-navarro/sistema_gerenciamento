64a3f01205e6dad9391dcc269ab9dcf604b743b6
vitor <vitorsantosnavarro@gmail.com>
2023-03-30 21:12:14 -0300

    regex implementado no telefone

--- src/utils/validators/phone.tsx
+++ src/utils/validators/phone.tsx
@@ -1,5 +1,5 @@
 export default function phone_validate(phone: string):boolean {
-  const phoneRegex = /(\(?\d{2}\)?\s)?(\d{4,5}\-\d{4})/;
+  const phoneRegex =  /^((\+)?[1-9]{2}(\s)?)?((\()?[1-9][0-9](\))?(\s)?)?([9]{1})?([1-9]{1})[0-9]{3}([-]?[0-9]{4})$/;
 
   if (typeof phone === 'string' && phoneRegex.test(phone)) {
     return true;
