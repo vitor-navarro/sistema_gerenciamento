a04b0e87deb4280301400a96ab3a92a9115eb6d3
vitor <vitorsantosnavarro@gmail.com>
2023-04-02 16:21:30 -0300

    validators email

--- src/utils/validators/email.tsx
+++ src/utils/validators/email.tsx
@@ -1,5 +1,5 @@
 export default function email_validate(email : string) : boolean{
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (email && !emailRegex.test(email)) {
         return false;
       }
