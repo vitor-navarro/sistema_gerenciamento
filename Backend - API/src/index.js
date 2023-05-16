514a55008d3af243feb836814e45e005606e93ea
vitor-navarro <vitorsantosnavarro@gmail.com>
2023-05-16 08:04:02 -0300

    Organizado o index

--- Backend - API/src/index.js
+++ Backend - API/src/index.js
@@ -1,56 +1,47 @@
 require('dotenv').config()
 
+//imports
 const express = require('express');
 const session = require('express-session')
-const app = express();
-const port = process.env.PORT;
-
 const bodyParser = require('body-parser')
-
 const cors = require('cors')
-const corsOptions = {
-    origin: process.env.ENABLED_CORS
-}
-
+const swaggerUi = require("swagger-ui-express")
 const conn = require("./db/conn")
 
-app.use(
-    express.urlencoded({
-        extended: true,
-    }),
-)
-
-app.use(express.json())
-
-//cors
-app.use(cors(corsOptions))
-app.use(bodyParser.json())
-
-//express session
-app.use(
-    session({
-        secret: process.env.SESSIONS_SECRET,
-        resave: false,
-        saveUninitialized: false,
-    })
-)
-
-//swagger docs
-const swaggerUi = require("swagger-ui-express")
-const swaggerDocs = require("./swagger.json")
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup())
-
+//routes imports
 const emailsRoutes = require("./routes/email")
 const userRoutes = require("./routes/userRoutes")
 const authRoutes = require("./routes/authRoutes")
 
+//initialization
+const app = express();
+const port = process.env.PORT;
+
+
+
+//Middlewares
+app.use(cors({
+    origin: process.env.ENABLED_CORS
+}))
+app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: true }),)
+app.use(express.json())
+app.use(session({
+    secret: process.env.SESSIONS_SECRET,
+    resave: false,
+    saveUninitialized: false,
+}))
+
+//swagger docs
+const swaggerDocs = require("./swagger.json")
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup())
+
+//Routes
 app.use("/emails", emailsRoutes)
 app.use("/user", userRoutes)
 app.use("/auth", authRoutes)
 
-conn
-    .sync()
+conn.sync()
     .then(()=>{
     app.listen(port, () => {
             console.log(`Server is listening on port http://localhost:${port}`);
