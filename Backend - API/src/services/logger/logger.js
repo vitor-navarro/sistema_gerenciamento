89098e57828773cd9d8034a696ea22a59934dd53
vitor-navarro <vitorsantosnavarro@gmail.com>
2023-05-22 08:38:49 -0300

    add minimal doc to logger

--- Backend - API/src/services/logger/logger.js
+++ Backend - API/src/services/logger/logger.js
@@ -17,8 +17,20 @@ async function gravar(data) {
 	}
 }
 
+/**
+ * Constate
+ * Possibilities: serious, error, warning, info, performance, other
+*/
+
 const log_data = {
-	// log destined to know if someone is trying to circument the system
+	/**
+	 * ACCEPTS AN OBJECT with the arguments: message error(optional) path
+	 * log destined to know if someone is trying to circument the system
+	 * @property {string} data.message - A message to log.
+	 * @property {string} data.[error] - OPTIONAL - string Error to log in database.
+	 * @property {string} data.path - The current path that is in the route manager: example user/addUser.
+	 * @returns {Object} log
+	 */
 	serious: async function (data) {
 		if (!data.message) {
 			data.message = "Inserido sem mensagem verificar"
@@ -45,6 +57,12 @@ const log_data = {
 		}
 	},
 
+	/**
+	 * ACCEPTS AN OBJECT with the arguments: message error
+	 * @property {string} data.message - A message to log.
+	 * @property {string} data.error - string Error to log in database.
+	 * @returns {Object} log
+	 */
 	error: async function (data) {
 		if (!data.message) {
 			data.message = "Inserido sem mensagem verificar"
@@ -69,6 +87,12 @@ const log_data = {
 		}
 	},
 
+	/**
+	 * ACCEPTS AN OBJECT with the arguments: message error(optional)
+	 * @property {string} data.message - A message to log.
+	 * @property {string} data.[error] - OPTIONAL - string Error to log in database.
+	 * @returns {Object} log
+	 */
 	warning: async function (data) {
 		if (!data.message) {
 			data.message = "Inserido sem mensagem verificar"
@@ -88,6 +112,11 @@ const log_data = {
 		}
 	},
 
+	/**
+	 * ACCEPTS AN OBJECT with the arguments: message
+	 * @property {string} data.message - A message to log.
+	 * @returns {Object} log
+	 */
 	info: async function (data) {
 		if (!data.message) {
 			data.message = "Inserido sem mensagem verificar"
@@ -105,8 +134,13 @@ const log_data = {
 			throw error
 		}
 	},
-
-	//For monitoring if you need it, or you want to know how fast the system is
+	/**
+	 * ACCEPTS AN OBJECT with the arguments: message path
+	 * For monitoring if you need it, or you want to know how fast the system is
+	 * @property {string} data.message - A message to log.
+	 * @property {string} data.path - The current path that is in the route manager: example user/addUser.
+	 * @returns {Object} log
+	 */
 	performance: async function (data) {
 		if (!data.message) {
 			data.message = "Inserido sem mensagem verificar"
@@ -131,7 +165,14 @@ const log_data = {
 		}
 	},
 
-	// importance level 1 or 0 please
+			/**
+	 * ACCEPTS AN OBJECT with the arguments: message importanceLevel aditional info
+	 * @property {string} data.message - A message to log.
+	 * @property {number} [data.importance_level] - (OPTIONAL) 1 or 0 default 0. if it is 1 it will not be excluded, if 0 it will be excluded within 7 days after the weekly automated report
+	 * @property {string} data.aditionalInfo - Additional information for the log
+	 * @returns {Object} log
+	 */
+
 	other: async function (data) {
 		if (!data.message) {
 			data.message = "Inserido sem mensagem verificar"
@@ -143,6 +184,15 @@ const log_data = {
 			console.log("inserido sem importance_level")
 		}
 
+		if (data.importance_level === 1 || data.importance_level === 0) {
+			data.importance_level = data.importance_level.toString();
+		  } else if (data.importance_level === "1" || data.importance_level === "0") {
+			data.importance_level = data.importance_level
+		  } else {
+			console.log("Importance level não é 0 nem 1.");
+		  
+			data.importance_level = "1 - " + data.importance_level;
+		  }
 
 		if (!data.aditionalInfo) {
 			data.aditionalInfo = "Inserido sem aditionalInfo verificar"
